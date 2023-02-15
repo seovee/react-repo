5f97b7ff567eaed69e0a71cd4b41860b4e6c137a
김진섭 <18073169+seovee@users.noreply.github.com>
2023-02-15 13:15:38 +0900

    Delete 01. React-기초 directory

--- 01. React-기초/frontend/src/learn/likereact.script.js
+++
@@ -1,7 +0,0 @@
-import { createElement as h, render } from './likeReact/index.js';
-
-const app = h('div', {
-  className: 'app'
-}, h('h1', null, 'Hello Virtual DOM'));
-
-render(app, document.getElementById('root'));
--- 01. React-기초/frontend/src/main.js
+++
@@ -1,6 +0,0 @@
-// import './utils/supportJavaScript.js';
-// import './learn/vanilla.script.js';
-// import './learn/likereact.script.js';
-import './learn/react.script.js';
-
-
